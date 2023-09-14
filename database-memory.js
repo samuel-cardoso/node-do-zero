2a5fb1f25c20ff0b66a3834c29b7ccc92d698839
Samuel Cardoso da Silva <samuelcardoso564@gmail.com>
2023-09-14 17:01:27 -0300

    feat: projeto api node

---
+++ database-memory.js
@@ -0,0 +1,41 @@
+import { randomUUID } from "node:crypto"
+
+export class DatabaseMemory {
+    #videos = new Map()
+
+    // Set, Map
+
+    list(search) {
+        return Array.from(this.#videos.entries())
+        .map((videoArray) => {
+            const id = videoArray[0]
+            const data = videoArray[1]
+            return {
+                id,
+                ...data,
+            }
+        })
+        .filter(video => {
+            if (search) {
+                return video.title.includes(search)
+            }
+
+            return true
+        })
+    }
+    
+    create(video) {
+        const videoId = randomUUID()
+        // UUDI - Universal Unique ID
+
+        this.#videos.set(videoId, video)
+    }
+
+    update(id, video) {
+        this.#videos.set(id, video)
+    }
+
+    delete(id, video) {
+        this.#videos.delete(id, video)
+    }
+}
--- server.js
+++ server.js
@@ -37,18 +37,62 @@ server.listen(3333)
 
 import { fastify } from "fastify"
 
+import { DatabaseMemory } from "./database-memory.js" 
+
 const server = fastify()
 
-server.get('/', () => {
-    return 'Hello World'
+const database = new DatabaseMemory()
+
+// CRUD -> Create, Read, Update, Delete
+
+// POST http://localhost:3333/videos 
+// PUT htrp://localhost:3333/videos/3
+
+// Route parameter
+ 
+
+// Request Body 
+
+server.post('/videos', (request, reply) => {
+    const {title, description, duration} = request.body
+    
+    database.create({
+        title,
+        description,
+        duration, 
+    }) 
+    
+    return reply.status(201).send()
 })
 
-server.get('/hello', () => {
-    return 'Hello Rocketseat'
+server.get('/videos', (request) => {
+    const search = request.query.search    
+    
+    const videos = database.list(search)
+
+    return videos
 })
 
-server.get('/node', () => {
-    return 'Hello Node.js'
+server.put('/videos/:id', (request, reply) => {
+    const videoId = request.params.id
+    const {title, description, duration} = request.body
+    
+    database.update(videoId, {
+        title,
+        description,
+        duration,
+    })
+
+    return reply.status(204).send()
+})
+
+server.delete('/videos/:id', (request, reply) => {
+    const videoId = request.params.id
+
+    database.delete(videoId)
+
+    return reply.status(204).send()
+
 })
 
 server.listen({
