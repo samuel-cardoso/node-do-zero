2c95ef615daa954a6c7660c4fc79ef56cd0b4c0d
Samuel Cardoso da Silva <samuelcardoso564@gmail.com>
2023-09-18 13:33:37 -0300

    feat: conectando banco de dados

--- server.js
+++ server.js
@@ -37,11 +37,12 @@ server.listen(3333)
 
 import { fastify } from "fastify"
 
-import { DatabaseMemory } from "./database-memory.js" 
+// import { DatabaseMemory } from "./database-memory.js" 
+import { DatabasePostgres } from "./database-postgres.js"
 
 const server = fastify()
 
-const database = new DatabaseMemory()
+const database = new DatabasePostgres()
 
 // CRUD -> Create, Read, Update, Delete
 
@@ -53,10 +54,10 @@ const database = new DatabaseMemory()
 
 // Request Body 
 
-server.post('/videos', (request, reply) => {
+server.post('/videos', async (request, reply) => {
     const {title, description, duration} = request.body
     
-    database.create({
+    await database.create({
         title,
         description,
         duration, 
@@ -65,19 +66,19 @@ server.post('/videos', (request, reply) => {
     return reply.status(201).send()
 })
 
-server.get('/videos', (request) => {
+server.get('/videos', async (request) => {
     const search = request.query.search    
     
-    const videos = database.list(search)
+    const videos = await database.list(search)
 
     return videos
 })
 
-server.put('/videos/:id', (request, reply) => {
+server.put('/videos/:id', async (request, reply) => {
     const videoId = request.params.id
     const {title, description, duration} = request.body
     
-    database.update(videoId, {
+    await database.update(videoId, {
         title,
         description,
         duration,
@@ -86,7 +87,7 @@ server.put('/videos/:id', (request, reply) => {
     return reply.status(204).send()
 })
 
-server.delete('/videos/:id', (request, reply) => {
+server.delete('/videos/:id', async (request, reply) => {
     const videoId = request.params.id
 
     database.delete(videoId)
@@ -96,5 +97,5 @@ server.delete('/videos/:id', (request, reply) => {
 })
 
 server.listen({
-    port: 3333,
+    port: process.env.PORT ?? 3333,
 })
